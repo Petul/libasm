680b07d6badbde59c71c467bc73e05bbca7aad45
Peter <leander.peter@gmail.com>
2024-06-25 17:14:27 +0300

    Fix changing the rbx register

--- ft_strlen.asm
+++ ft_strlen.asm
@@ -6,7 +6,7 @@
 ;    By: pleander <pleander@student.hive.fi>        +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2024/06/17 21:02:07 by pleander          #+#    #+#              ;
-;    Updated: 2024/06/17 21:02:09 by pleander         ###   ########.fr        ;
+;    Updated: 2024/06/25 17:14:09 by pleander         ###   ########.fr        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
@@ -15,13 +15,12 @@ section	.text
 ;input rdi as pointer to string
 ;output rax as 64 bit integer
 ft_strlen:
-	mov rbx, 0 ; store length counter in rbx
+	mov rax, 0 ; store length counter in rbx
 _count_loop:
-	inc rbx ; increment counter
+	inc rax ; increment counter
 	inc rdi ; increment C string pointer
 	mov cl, [rdi] ; copy value of rdi to cl (to get char size)
 	cmp cl, 0 ; check if null
 	jne _count_loop
 
-	mov rax, rbx ; copy counter to rax and return
 	ret
