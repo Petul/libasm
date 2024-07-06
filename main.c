8df4d8496f35e701c6f0c5ad5c3bfa49f71c2e6a
Peter <leander.peter@gmail.com>
2024-07-06 18:20:55 +0300

    Update main

--- main.c
+++ main.c
@@ -110,11 +110,11 @@ void test_ft_strdup(void)
 int main(void)
 {
 	test_ft_strlen();
-	// test_ft_strcpy();
-	// test_ft_strcmp();
-	// test_ft_write();
-	// test_ft_read();
-	// test_ft_strdup();
+	test_ft_strcpy();
+	test_ft_strcmp();
+	test_ft_write();
+	test_ft_read();
+	test_ft_strdup();
 
 	return (0);
 }
